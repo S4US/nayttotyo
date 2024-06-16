f26203905b14676f0a9cdd25ba7c4c2a4d310dc6
Sasu <sipolansasu@gmail.com>
2024-06-16 19:29:31 +0300

    Improved palvelut site

--- js/draggable-svg.js
+++ js/draggable-svg.js
@@ -3,14 +3,14 @@ let draggable = null;
 
 $('#lock').change(function () {
     if ($(this).is(':checked')) {
-        console.log("Checkbox is checked..")
+        console.log("checked")
         if (draggable != null) {
             draggable.remove();
             draggable = null;
             console.log(draggable);
         }
     } else {
-        console.log("Checkbox is not checked..")
+        console.log("not checked..")
         if (draggable == null) {
             draggable = new PlainDraggable(sohva);
             console.log(draggable);
