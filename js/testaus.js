2defb1e1c2595fc73b03631e3ad8252b6a2b92d8
Sasu <sipolansasu@gmail.com>
2024-06-17 08:09:19 +0300

    Update

--- js/testaus.js
+++ js/testaus.js
@@ -1,15 +1,21 @@
 $(document).ready(function () {
+    // Nappulat
     const box1_button = document.getElementById("box1-button");
     const box2_button = document.getElementById("box2-button");
+
+    // Vaihdettava sohva elementti
     var imgElement = document.getElementById("sohva");
 
+    // Eri sohva svg
     let svgArray = ["svg/sohva1.svg", "svg/sohva2.svg", "svg/sohva3.svg", "svg/sohva4.svg"];
     let current_index = 0;
 
+    // Piilota elementti
     $("#sohva").hide();
 
     box1_button.disabled = true;
 
+    // Napit joilla vaihdetaan kumman laatikon väriä vaihdetaan
     $("#box1-button").click(function () {
         box1_button.disabled = true;
         box2_button.disabled = false;
@@ -20,6 +26,7 @@ $(document).ready(function () {
         box2_button.disabled = true;
     });
 
+    // Värin vaihto napit
     $(".nappula").click(function () {
         if (box1_button.disabled && !box2_button.disabled) {
             $("#box1").css({
@@ -32,10 +39,12 @@ $(document).ready(function () {
         }
     });
 
+    // Näytä sohva nappi
     $("#sohva-nappi").click(function () {
         $("#sohva").toggle();
     });
 
+    // Nuolet joilla vaihdetaan sohvaa
     $("#left").on("click", function() {
         if (current_index == 0) {
             current_index = svgArray.length - 1;
